120f371cf3eeb427d74ab43a3d64233e9211ab83
lajbel <lajbelms@gmail.com>
2024-11-18 15:19:09 -0300

    feat: update messages on create and build

--- src/template/vite.config.js
+++ src/template/vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from "vite";
 
+const kaplayCongrats = () => {
+    return {
+        name: 'vite-plugin-kaplay-hello',
+        buildEnd() {
+            const line = "---------------------------------------------------------";
+            const msg = `🦖 Awesome pal! Send your game to us:\n\n💎 Discord: https://discord.com/invite/aQ6RuQm3TF \n💖 Donate to KAPLAY: https://opencollective.com/kaplay\n\ (you can disable this msg on vite.config)`;
+            process.stdout.write(`${line}\n${msg}\n${line}\n`);
+            
+            process.stdout.write(`✨ Done ✨\n`);
+        }
+    }
+}   
+
+
 export default defineConfig({
     // index.html out file will start with a relative path for script
     base: "./",
     build: {
-        // disable this for low bundle size
+        // disable this for low bundle sizes
         sourcemap: true,
     },
+    port: 3001,
+    plugins: [
+        // Disable messages removing this line
+        kaplayCongrats(),
+    ],
+
 });
