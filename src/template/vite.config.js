cfb71dd98b920d2d502792c4a093354f7a0c19d3
lajbel <lajbelms@gmail.com>
2024-11-18 15:20:08 -0300

    chore: update msgs

--- src/template/vite.config.js
+++ src/template/vite.config.js
@@ -6,9 +6,8 @@ const kaplayCongrats = () => {
         buildEnd() {
             const line = "---------------------------------------------------------";
             const msg = `🦖 Awesome pal! Send your game to us:\n\n💎 Discord: https://discord.com/invite/aQ6RuQm3TF \n💖 Donate to KAPLAY: https://opencollective.com/kaplay\n\ (you can disable this msg on vite.config)`;
-            process.stdout.write(`${line}\n${msg}\n${line}\n`);
-            
-            process.stdout.write(`✨ Done ✨\n`);
+
+            process.stdout.write(`\n${line}\n${msg}\n${line}\n`);
         }
     }
 }   
